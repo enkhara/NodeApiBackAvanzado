a94217030a4410738624eef48a7ca66a912a12bb
Georgina Torralba <georgina.torralba@gmail.com>
2021-05-06 22:54:45 +0200

    Use .env

--- model/connectToMongoose.js
+++ model/connectToMongoose.js
@@ -3,18 +3,19 @@
 const mongoose = require('mongoose');
 
 //shut down app
-mongoose.connection.on('error', err => {
-    console.log('Connection to the database failed ', err);
-    process.exit(1);
+mongoose.connection.on('error', (err) => {
+	console.log('Connection to the database failed ', err);
+	process.exit(1);
 });
 
 mongoose.connection.once('open', () => {
-    console.log('Connection to MongoDB successful', mongoose.connection.name);
+	console.log('Connection to MongoDB successful', mongoose.connection.name);
 });
 
-mongoose.connect('mongodb://localhost/nodepop', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+mongoose.connect(process.env.MONGODB_CONNECTION_STR, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	//useCreateIndex: true
 });
 
 module.exports = mongoose.connection;
